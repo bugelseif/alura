f2fccd16fa6c7ea5c15d61ec0cabcc317941978c
Bruna <bferreira.slv@gmail.com>
2022-03-09 03:03:16 -0300

    condição range de notas

--- media.js
+++ media.js
@@ -2,11 +2,16 @@ const botao = document.getElementById("calcular");
 botao.addEventListener("click", media)
 
 function media(){
-    const n1 = parseFloat(document.getElementById("nota1").value);
-    const n2 = parseFloat(document.getElementById("nota2").value);
-    const n3 = parseFloat(document.getElementById("nota3").value);
-    const n4 = parseFloat(document.getElementById("nota4").value);
-
-    const resultado = (n1+n2+n3+n4)/4;
-    document.getElementsByTagName("h2")[1].innerHTML = resultado
+    const notas = document.querySelectorAll(".notas")
+    let soma = 0;
+    
+    for (let i = 0; i < notas.length; i++) {
+        if(notas[i].value>10 || notas[i].value<0 || notas[i].value===''){
+            document.getElementsByTagName("h2")[1].innerHTML = "Reveja as notas";
+            break;
+        }else{
+        soma += parseFloat(notas[i].value);
+        document.getElementsByTagName("h2")[1].innerHTML = "A média é: " + soma/4;
+        }
+    }        
 }
