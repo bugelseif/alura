c0b0de2e8a741a2ca1b37b84c6786f3862bf250e
Bruna <bferreira.slv@gmail.com>
2022-03-13 02:21:42 -0300

    ajuste conversor

--- conversor.js
+++ conversor.js
@@ -3,7 +3,6 @@ botao.addEventListener("click", converte)
 
 function converte(){
     const valor = parseFloat(document.querySelector("#valor").value)
-    console.log(valor)
     if(!!valor){
         document.getElementById("valorConvertido").innerHTML = "O valor em R$ " +(valor*5)
     }else{
